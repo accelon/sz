a7df7c7232cbb557c312e044369b1199ed21f07e
yap cheah shen <yapcheahshen@gmail.com>
2024-03-13 16:37:30 +0800

    fix cannot offline

--- src/appstore.js
+++ src/appstore.js
@@ -2,7 +2,7 @@ export * from 'accelon23/src/store.js';
 import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
 
 _ACC23.CacheName='v1::sz'
-_ACC23.AppVer='23.11.27';
+_ACC23.AppVer='24.3.12';
 _ACC23.AppName='sz',
 _ACC23.AppTitle='平行藏'
 _ACC23.repoLink="https://github.com/accelon/sz/"
@@ -11,12 +11,12 @@ _ACC23.selectedptks=['sc','cs-xsq','cs-yh'];
 _ACC23.activeptk='cs-xsq';
 _ACC23.MAXFAVORITE=100;
 _ACC23.ITEMPERPAGE=5;
-_ACC23.favorites=["d24.6:4","d16.218:2","m3.29:6","m142.379:30","m106.73:13","s56.1071:3","a2.21"];
+_ACC23.favorites=["d24.6:4","d16.218:2","m3.29:6","m32.333:6","m142.379:30","m106.73:13","s56.1071:3","a2.21"];
 _ACC23.painters={};
 _ACC23.ptkinfo={
         'cs':'巴利原文(第六次結集)24MB',
         'sc':'SuttaCentral英譯13MB',
-        'cs-ccc':'莊春江11MB',
+        'cs-ccc':'莊春江13MB',
         'cs-hz':'巴利佛典譯叢(杭州佛學院)6MB',
         'cs-xsq':'蕭式球11MB',
         'cs-yh':'南傳大藏經(元亨寺)10MB',
@@ -24,7 +24,7 @@ _ACC23.ptkinfo={
         'cs-bb':'菩提比丘等11MB',
         'cs-kmj':'日本語（光明寺経藏）20MB',
         'cs-pku':'北大法胜 2MB',
-        'cs-mm':'緬譯ဗမာစာ(緬甸宗教局)18MB',
+        'cs-mm':'緬譯ဗမာစာ(緬甸宗教局)29MB',
 }
 
 export const ACC23=_ACC23;
